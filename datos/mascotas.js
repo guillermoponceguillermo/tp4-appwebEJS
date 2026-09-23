52d64bf74c4113450ff12ecd211184bfda684078
guillermoponceguillermo <112491446+guillermoponceguillermo@users.noreply.github.com>
2026-09-23 11:14:29 -0300

    Add files via upload

---
+++ datos/mascotas.js
@@ -0,0 +1,47 @@
+[
+  {
+    "id": 1,
+    "nombre": "Firulais",
+    "especie": "Perro",
+    "edad": 3,
+    "descripcion": "Perro muy juguetón y amigable, ideal para familias con niños.",
+    "estado": "En adopción",
+    "imagen": "/img/mascota.svg"
+  },
+  {
+    "id": 2,
+    "nombre": "Michi",
+    "especie": "Gato",
+    "edad": 2,
+    "descripcion": "Tranquilo, cariñoso y le encanta dormir al sol.",
+    "estado": "Reservada",
+    "imagen": "/img/mascota.svg"
+  },
+  {
+    "id": 3,
+    "nombre": "Rex",
+    "especie": "Perro",
+    "edad": 5,
+    "descripcion": "Guardian fiel, educado y obediente.",
+    "estado": "Adoptada",
+    "imagen": "/img/mascota.svg"
+  },
+  {
+    "id": 4,
+    "nombre": "Luna",
+    "especie": "Gato",
+    "edad": 1,
+    "descripcion": "Gata muy curiosa, llena de energía y compañía.",
+    "estado": "En adopción",
+    "imagen": "/img/mascota.svg"
+  },
+  {
+    "id": 5,
+    "nombre": "Pelusa",
+    "especie": "Conejo",
+    "edad": 0,
+    "descripcion": "Pequeña mascota dócil de hábitos tranquilos.",
+    "estado": "En adopción",
+    "imagen": "/img/mascota.svg"
+  }
+]
